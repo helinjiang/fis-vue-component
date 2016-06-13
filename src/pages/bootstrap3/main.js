6d588f3b71dbd291cb9a64f3e196f62663907837
helinjiang <me@helinjiang.com>
2016-06-13 20:24:35 +0800

    改写引入main.js的方式

--- src/pages/bootstrap3/main.js
+++ src/pages/bootstrap3/main.js
@@ -2,25 +2,29 @@ import Vue from '/common/lib/vue'
 import VueRouter from '/common/lib/vue-router'
 import {configRouter} from './route-config'
 
-// install router
-Vue.use(VueRouter);
+export default {
+    init() {
+        // install router
+        Vue.use(VueRouter);
 
-// create router
-const router = new VueRouter({
-    history: true,
-    saveScrollPosition: true
-});
+        // create router
+        const router = new VueRouter({
+            // history: true,
+            saveScrollPosition: true
+        });
 
-// configure router
-configRouter(router);
+        // configure router
+        configRouter(router);
 
-// boostrap the app
-const App = Vue.extend({});
+        // boostrap the app
+        const App = Vue.extend({});
 
-// now we can start the app!
-// router will create an instance of App and mount to
-// the element matching the selector #app.
-router.start(App, '#app');
+        // now we can start the app!
+        // router will create an instance of App and mount to
+        // the element matching the selector #app.
+        router.start(App, '#app');
 
-// just for debugging
-window.router = router;
+        // just for debugging
+        window.router = router;
+    }
+}
