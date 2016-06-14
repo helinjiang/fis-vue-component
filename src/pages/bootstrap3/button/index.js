ed258eb0e6c52943d5b7b3aeb0f3a606ba9fa0df
helinjiang <me@helinjiang.com>
2016-06-14 13:22:13 +0800

    布局样式

--- src/pages/bootstrap3/button/index.js
+++ src/pages/bootstrap3/button/index.js
@@ -1,11 +1,11 @@
 import BaseMixin from '../_mixin/baseMixin.js'
-import Button from '/components/bootstrap3/button'
+import Bs3Button from '/components/bootstrap3/button'
 
 export default {
     template: __inline('main.html'),
     mixins: [BaseMixin],
     components: {
-        Button
+        Bs3Button
     },
     methods: {
         clickButton() {
