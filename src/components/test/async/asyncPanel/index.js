acfd3fe85b19fad3e5035dfa9faeab58107a2a06
helinjiang <me@helinjiang.com>
2016-06-19 21:56:20 +0800

    异步加载组件的css资源

--- src/components/test/async/asyncPanel/index.js
+++ src/components/test/async/asyncPanel/index.js
@@ -1,8 +1,10 @@
 /**
  * 异步的方式引入其css文件
  * https://github.com/fex-team/mod
+ *
+ * 但这种方式有个缺陷，就是必须要加载完js之后，才会去加载css
  */
-require.loadCss ({url: __uri('./main.css')});
+// require.loadCss({url: __uri('./main.scss')});
 
 /**
  * 如果该css文件不大，也可以选择将css和其他文件一起打包，提前下载，只需要申明@require main.css即可。
--- src/pages/test/async/index.js
+++ src/pages/test/async/index.js
@@ -1,10 +1,27 @@
+require.resourceMap({
+    res: {
+        asyncPanel: {
+            url: __uri('/components/test/async/asyncPanel'),
+            // deps: ['asyncPanel_css'],
+            // isMod: false
+        },
+        asyncPanel_css: {
+            url: __uri('/components/test/async/asyncPanel/main.scss'),
+            deps: [],
+            type: 'css'
+        }
+    },
+    pkg: {}
+});
+
 export default {
     template: __inline('main.html'),
     components: {
         asyncPanel: (resolve)=> {
             setTimeout(()=> {
-                console.log('---begin---');
-                require(['/components/test/async/asyncPanel'], function (main) {
+                console.log('---begin-3--');
+                // require(['/components/test/async/asyncPanel'], function (main) {
+                require(['asyncPanel'], function (main) {
                     console.log('=========main==========', main);
                     resolve(main);
                 });
